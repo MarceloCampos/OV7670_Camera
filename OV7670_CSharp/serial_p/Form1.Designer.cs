e08ea7c79a852938068fe9057e5987d1ca83b13c
Marcelo Campos <marcelo.camposs182@gmail.com>
2017-06-23 21:42:46 -0300

    Add files via upload

---
+++ OV7670_CSharp/serial_p/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace serial_p
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.pctBox = new System.Windows.Forms.PictureBox();
+            this.txtComPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtBaudRate = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblResol = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 1000000;
+            this.serialPort1.DtrEnable = true;
+            this.serialPort1.PortName = "COM16";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.SerialREceive);
+            // 
+            // pctBox
+            // 
+            this.pctBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pctBox.Location = new System.Drawing.Point(22, 42);
+            this.pctBox.Name = "pctBox";
+            this.pctBox.Size = new System.Drawing.Size(320, 240);
+            this.pctBox.TabIndex = 0;
+            this.pctBox.TabStop = false;
+            // 
+            // txtComPort
+            // 
+            this.txtComPort.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtComPort.Location = new System.Drawing.Point(358, 61);
+            this.txtComPort.Name = "txtComPort";
+            this.txtComPort.Size = new System.Drawing.Size(100, 20);
+            this.txtComPort.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(358, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Abri Porta COM";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtBaudRate
+            // 
+            this.txtBaudRate.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBaudRate.Location = new System.Drawing.Point(358, 103);
+            this.txtBaudRate.Name = "txtBaudRate";
+            this.txtBaudRate.Size = new System.Drawing.Size(100, 20);
+            this.txtBaudRate.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(355, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "COM Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(355, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baud Rate";
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(355, 236);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(74, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Aguardando...";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(355, 259);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(40, 13);
+            this.lblTime.TabIndex = 7;
+            this.lblTime.Text = "Tempo";
+            // 
+            // lblResol
+            // 
+            this.lblResol.AutoSize = true;
+            this.lblResol.Location = new System.Drawing.Point(355, 282);
+            this.lblResol.Name = "lblResol";
+            this.lblResol.Size = new System.Drawing.Size(64, 13);
+            this.lblResol.TabIndex = 8;
+            this.lblResol.Text = "Resolução: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 342);
+            this.Controls.Add(this.lblResol);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtBaudRate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtComPort);
+            this.Controls.Add(this.pctBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pctBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.PictureBox pctBox;
+        private System.Windows.Forms.TextBox txtComPort;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtBaudRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblResol;
+    }
+}
+
---
+++ OV7670_CSharp/serial_p/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace serial_p
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static int width = 320;
+        static int height = 240;
+        static int Start_StopSize = 10;
+        static int Control_Bytes = 0;
+        static string StartString = "*RDY*";
+        static string StopString = "*STP*";
+
+
+        TimeSpan Start_Time;
+        TimeSpan Stop_Time;
+
+        public static byte RxBytes;
+
+        public bool StartFlag = false;
+
+        byte[] buffer = new byte[width * height + Start_StopSize + Control_Bytes];
+        int bytesJaLidos = 0;
+        int bytesParaLer;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            serialPort1.PortName = Properties.Settings.Default.CommPort;
+            serialPort1.BaudRate = Properties.Settings.Default.BaudRate;
+            txtBaudRate.Text = Properties.Settings.Default.BaudRate.ToString();
+            txtComPort.Text = Properties.Settings.Default.CommPort.ToUpper();
+
+            serialPort1.Open();
+        }
+
+        private void SerialREceive(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                while (true)
+                {
+                    bytesParaLer = serialPort1.BytesToRead;
+                    bytesJaLidos += serialPort1.Read(buffer, bytesJaLidos, bytesParaLer);
+
+                    if (bytesParaLer == 0)
+                        break;
+                }
+
+                string RxString = Encoding.ASCII.GetString(buffer);
+                int xp = RxString.Length;
+
+                if (!StartFlag && RxString.StartsWith(StartString))
+                {
+                    StartFlag = true;
+                    Debug.Print("Start !!");
+                    Start_Time = DateTime.Now.TimeOfDay;
+                    //                lblStatus.Text = ("Recebendo...");
+                    //                this.Refresh();
+                    //               lblStatus.Refresh();
+                }
+                else if (StartFlag && RxString.EndsWith(StopString))
+                {
+                    StartFlag = false;
+                    bytesJaLidos = 0;
+                    Debug.Print("Stop !!");
+                    //              lblStatus.Text = ("Aguardando Imagem...");
+                    Stop_Time = DateTime.Now.TimeOfDay;
+
+                    TimeSpan time_rx = Stop_Time-Start_Time;
+                    Debug.Print("Tempo de Leitura: " + time_rx.ToString());
+
+                    //               lblTime.Text = "Tempo: " + time_rx.ToString();
+
+                    //this.Refresh();
+                    //               lblStatus.Refresh();
+                    //               lblTime.Refresh();
+
+                    Analiza_Rx();
+                }
+                else if (!StartFlag)
+                {
+                    bytesJaLidos = 0;
+                }
+
+            }
+            catch (Exception ix)
+            {
+                Debug.Print("Erro leitura ");
+                //       lblStatus.Text = ("Erro leitura ");
+                StartFlag = false;
+                bytesJaLidos = 0;
+            }
+
+        }
+
+        private void Analiza_Rx()
+        {
+            byte[] rawBytes = new byte[width * height];
+
+            for (int i = 0; i < rawBytes.Length; i++)
+            {
+                rawBytes[i] = buffer[i + StartString.Length + Control_Bytes];
+            }
+
+            var ms = new System.IO.MemoryStream(rawBytes);
+
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format8bppIndexed);
+            BitmapData bitmapData = bitmap.LockBits(new Rectangle(0, 0, width, height),
+                                            ImageLockMode.WriteOnly, bitmap.PixelFormat);
+
+            Marshal.Copy(rawBytes, 0, bitmapData.Scan0, rawBytes.Length);
+            bitmap.UnlockBits(bitmapData);
+
+            var pal = bitmap.Palette;
+            for (int i = 0; i < 256; i++) pal.Entries[i] = Color.FromArgb(i, i, i);
+            bitmap.Palette = pal;
+
+            Array.Clear(buffer, 0, buffer.Length);
+
+            pctBox.Image = bitmap;
+           // lblResol.Text = "Resolução: " + width.ToString() + " x " + height.ToString();
+            //lblResol.Refresh();
+        }
+
+        static byte[] PadLines(byte[] bytes, int rows, int columns)
+        {
+            int currentStride = columns; // 3
+            int newStride = columns;  // 4
+            byte[] newBytes = new byte[newStride * rows];
+            for (int i = 0; i < rows; i++)
+                Buffer.BlockCopy(bytes, currentStride * i, newBytes, newStride * i, currentStride);
+            return newBytes;
+        }
+
+    }
+}
